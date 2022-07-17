aeaea58b8a157cfae631f85c4271d353cfa0bf20
Alex Pullinger <alex.pullinger@gmail.com>
2022-07-17 17:28:44 +0100

    navigation table functionality

--- Resources/script.js
+++ Resources/script.js
@@ -1,11 +1,19 @@
 const navButton = document.getElementById("nav-button");
 const navTable = document.getElementById("navigation-table-list");
 const navTableList = document.getElementsByClassName("nav-link");
+let buttonClicked = false
 
 
 const navButtonClick = () => {
-    navTable.style.display = "flex";
-    navButton.innerHTML = "<i class='fa fa-xmark fa-lg'></i>";
+    switch(buttonClicked){
+        case false : navTable.style.display = "flex"
+                     buttonClicked = true;
+        break;
+        case true : navTable.style.display = "none"
+                    buttonClicked = false
+    }
+    
+    
 };
 
 navButton.onclick = navButtonClick
