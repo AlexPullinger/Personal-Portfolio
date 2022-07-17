e9a096ed61120cf158458aa08d35cd51721b78d8
Alex Pullinger <alex.pullinger@gmail.com>
2022-07-17 18:39:40 +0100

    add extra js functionality

--- Resources/script.js
+++ Resources/script.js
@@ -1,8 +1,9 @@
+//nav button functionality
+
 const navButton = document.getElementById("nav-button");
 const navTable = document.getElementById("navigation-table-list");
 const navTableList = document.getElementsByClassName("nav-link");
 let buttonClicked = false
-
 
 const navButtonClick = () => {
     switch(buttonClicked){
@@ -17,3 +18,28 @@ const navButtonClick = () => {
 };
 
 navButton.onclick = navButtonClick
+
+//projects selected functionality
+
+const personalPortfolio = document.getElementById("personal-portfolio");
+const html = document.getElementById("html-image");
+const css = document.getElementById("css-image");
+const javascript = document.getElementById("javascript-image");
+const git = document.getElementById("git-image");
+const github = document.getElementById("github-image");
+
+const personalPortfolioMoused = () => {
+    html.style.filter = "grayscale(0%)";
+    css.style.filter = "grayscale(0%)";
+    javascript.style.filter = "grayscale(0%)";
+}
+
+const personalPortfolioMouseLeave = () => {
+    html.style.filter = "grayscale(100%)";
+    css.style.filter = "grayscale(100%)";
+    javascript.style.filter = "grayscale(100%)";
+}
+
+personalPortfolio.onmouseover = personalPortfolioMoused
+
+personalPortfolio.onmouseleave = personalPortfolioMouseLeave
